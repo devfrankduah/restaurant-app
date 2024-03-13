5d71a8f821305a655a9d6d4c3ac8fbc89e765a69
devfrankduah <frankduah0@gmail.com>
2024-03-13 20:57:37 +0000

    --error & loading view

---
+++ src/components/shared/ErrorView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="error-view">
+    <div>
+      <div class="">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          width="74"
+          height="74"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M18.364 18.364A9 9 0 0 0 5.636 5.636m12.728 12.728A9 9 0 0 1 5.636 5.636m12.728 12.728L5.636 5.636"
+          />
+        </svg>
+      </div>
+    </div>
+    <p>Error</p>
+    <p v-if="message">
+      {{ message }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  message?: string
+}>()
+</script>
+<style scoped>
+.error-view {
+  align-items: center;
+  position: fixed;
+  inset: 0;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ src/components/shared/LoadingView.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="loading-view">
+    <div role="status">
+      <div class="loader"></div>
+      <span class="loading">Loading...</span>
+    </div>
+    <div>
+      {{ title }}
+    </div>
+    <p>{{ loadingText }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  loadingText?: string
+  title?: string
+  titleClass?: string
+}>()
+</script>
+<style scoped>
+.loading-view {
+  margin: 20px;
+  align-items: center;
+  position: relative;
+  inset: 0;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.loader {
+  border: 16px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 16px solid #000000;
+  width: 100px;
+  height: 100px;
+  -webkit-animation: spin 2s linear infinite; 
+  animation: spin 2s linear infinite;
+  position: inherit;
+}
+
+@-webkit-keyframes spin {
+  0% {
+    -webkit-transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+  }
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
