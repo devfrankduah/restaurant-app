39950a00515b6bfcd9445f0345d88eb9e5979a21
devfrankduah <frankduah0@gmail.com>
2024-03-14 05:30:57 +0000

    --side bar

---
+++ src/components/SideMenu.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="column is-4-tablet is-3-desktop is-2-widescreen">
+      <nav class="menu">
+        <p class="menu-label">Menu</p>
+        <ul class="menu-list">
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/dishes">Dishes</router-link></li>
+        </ul>
+      </nav>
+    </div>
+  </template>
+  
+  <style></style>
